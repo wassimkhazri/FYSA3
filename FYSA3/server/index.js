b8dd8be490a21d779e30f3dfb4d952bd237a45f4
Yasmine <yasmine.ghouleme@gmail.com>
2020-12-15 02:17:49 +0100

    make it better

--- FYSA3/server/index.js
+++ FYSA3/server/index.js
@@ -189,6 +189,7 @@ app.put("/order/update", function (req, res) {
     }
   });
 });
-app.listen(3000, function () {
+var port = process.env.PORT || "3000";
+app.listen(port, function () {
   console.log("listening on port 3000!");
 });
